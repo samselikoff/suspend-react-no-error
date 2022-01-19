6a23021b59de29788fc561e1e89fc877563c86b1
Sam Selikoff <sam.selikoff@gmail.com>
2022-01-19 16:16:15 -0500

    wip

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import { suspend } from "suspend-react";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  suspend(async () => {
+    await fetch("http://api.foo.com/");
+  }, []);
+
+  return <p>hi</p>;
+  // return <Component {...pageProps} />
 }
 
-export default MyApp
+export default MyApp;
